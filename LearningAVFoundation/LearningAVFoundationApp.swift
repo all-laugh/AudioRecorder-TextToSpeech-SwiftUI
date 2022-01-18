f1d1b7a7fb070f2ad4e4bac09b372c9d8a9547fb
Xiao Quan 全笑 <58939349+all-laugh@users.noreply.github.com>
2022-01-18 13:31:33 -0500

    Initial Commit

---
+++ LearningAVFoundation/LearningAVFoundationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAVFoundationApp.swift
+//  LearningAVFoundation
+//
+//  Created by Xiao Quan on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAVFoundationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
